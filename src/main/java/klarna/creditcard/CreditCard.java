7edbcd2ac0d8f2a1ae9463530f4d385ad5cb106c
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-10 10:59:56 +0100

    Move the logic to an instance method

--- src/main/java/klarna/creditcard/CreditCard.java
+++ src/main/java/klarna/creditcard/CreditCard.java
@@ -1,10 +1,21 @@
 package klarna.creditcard;
 
 public class CreditCard {
-    public static String maskify(String creditCardNumber) {
-        if (creditCardNumber.length() < 6) {
-            return creditCardNumber;
+    private final String number;
+
+    private CreditCard(String number) {
+        this.number = number;
+    }
+
+    private String maskify() {
+        if (number.length() < 6) {
+            return number;
         }
         return "1#3456";
+    }
+
+    public static String maskify(String creditCardNumber) {
+        CreditCard card = new CreditCard(creditCardNumber);
+        return card.maskify();
     }
 }

4067ccd8e2ba5185c6659d82fbc0e8dec14c11b7
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-14 13:33:33 +0100

    Add tests for 10 - 20

--- src/test/java/klarna/ordinalnumbers/OrdinalNumbersTest.java
+++ src/test/java/klarna/ordinalnumbers/OrdinalNumbersTest.java
@@ -29,6 +29,17 @@ public class OrdinalNumbersTest {
         assertEquals("7th", Challenge.numberToOrdinal(7));
         assertEquals("8th", Challenge.numberToOrdinal(8));
         assertEquals("9th", Challenge.numberToOrdinal(9));
+        assertEquals("10th", Challenge.numberToOrdinal(10));
+        assertEquals("11th", Challenge.numberToOrdinal(11));
+        assertEquals("12th", Challenge.numberToOrdinal(12));
+        assertEquals("13th", Challenge.numberToOrdinal(13));
+        assertEquals("14th", Challenge.numberToOrdinal(14));
+        assertEquals("15th", Challenge.numberToOrdinal(15));
+        assertEquals("16th", Challenge.numberToOrdinal(16));
+        assertEquals("17th", Challenge.numberToOrdinal(17));
+        assertEquals("18th", Challenge.numberToOrdinal(18));
+        assertEquals("19th", Challenge.numberToOrdinal(19));
+        assertEquals("20th", Challenge.numberToOrdinal(20));
     }
 
     @Test

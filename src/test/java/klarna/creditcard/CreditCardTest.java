9195b3bba6bd575e5984def51d618f5afeab9949
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-14 11:53:38 +0100

    Add acceptance tests

--- src/test/java/klarna/creditcard/CreditCardTest.java
+++ src/test/java/klarna/creditcard/CreditCardTest.java
@@ -24,4 +24,16 @@ public class CreditCardTest {
     public void doesNotMaskEmptyString() {
         assertEquals("", CreditCard.maskify(""));
     }
+
+    @Test
+    public void acceptanceTests() {
+        assertEquals("4###########5616", CreditCard.maskify("4556364607935616"));
+        assertEquals("4###-####-####-5616", CreditCard.maskify("4556-3646-0793-5616"));
+        assertEquals("6######5616", CreditCard.maskify("64607935616"));
+        assertEquals("ABCD-EFGH-IJKLM-NOPQ", CreditCard.maskify("ABCD-EFGH-IJKLM-NOPQ"));
+        assertEquals("A#######BCDEFG89HI", CreditCard.maskify("A1234567BCDEFG89HI"));
+        assertEquals("12345", CreditCard.maskify("12345"));
+        assertEquals("", CreditCard.maskify(""));
+        assertEquals("Skippy", CreditCard.maskify("Skippy"));
+    }
 }

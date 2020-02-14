833931d0ccea94b783e33dfca42ed52109fc34ea
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-14 13:41:37 +0100

    Convert standalone if to else if

--- src/main/java/klarna/ordinalnumbers/Challenge.java
+++ src/main/java/klarna/ordinalnumbers/Challenge.java
@@ -9,24 +9,19 @@ public class Challenge {
         if (number == 1) {
             suffix = "st";
             return number + suffix;
-        }
-        if (number == 2) {
+        } else if (number == 2) {
             suffix = "nd";
             return number + suffix;
-        }
-        if (number == 3) {
+        } else if (number == 3) {
             suffix = "rd";
             return number + suffix;
-        }
-        if (number == 21) {
+        } else if (number == 21) {
             suffix = "st";
             return number + suffix;
-        }
-        if (number == 31) {
+        } else if (number == 31) {
             suffix = "st";
             return number + suffix;
-        }
-        if (number == 22) {
+        } else if (number == 22) {
             suffix = "nd";
             return number + suffix;
         } else {

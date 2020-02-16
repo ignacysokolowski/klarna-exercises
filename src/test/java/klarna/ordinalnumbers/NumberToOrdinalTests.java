16217c0294a683f760fa1723fee6edb40eb1587d
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2020-02-16 22:42:34 +0100

    Rename the test class

--- src/test/java/klarna/ordinalnumbers/NumberToOrdinalTests.java
+++ src/test/java/klarna/ordinalnumbers/NumberToOrdinalTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class OrdinalNumbersTest {
+public class NumberToOrdinalTests {
 
     @Test
     public void appendsStToNumbersEndingWithOne() {
